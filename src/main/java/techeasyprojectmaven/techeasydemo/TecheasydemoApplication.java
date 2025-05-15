0e7b76051efa113aa8b3fc592788a041c3ad1e1c
root <root@ip-172-31-20-3.ec2.internal>
2025-05-15 16:07:37 +0000

    Added image upload functionality

---
+++ src/main/java/techeasyprojectmaven/techeasydemo/TecheasydemoApplication.java
@@ -0,0 +1,13 @@
+package techeasyprojectmaven.techeasydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecheasydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecheasydemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/techeasyprojectmaven/techeasydemo/TecheasydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package techeasyprojectmaven.techeasydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecheasydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
